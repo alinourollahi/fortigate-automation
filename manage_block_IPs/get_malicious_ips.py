3704976d71566128c87557193fe165bfede42d30
ali.nourollahi <alinourollahi777@gmail.com>
2023-07-16 11:26:17 +0330

    Add function "get_malicious_IP_group_From_FG"
    this function prints the members of "Malicious_IPs" object_group from Fortigate firewall.

---
+++ manage_block_IPs/get_malicious_ips.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3 
+
+import ipaddress
+from lib2to3.pgen2 import token
+from os import confstr
+from xmlrpc.client import boolean
+import json
+import requests
+import urllib3
+import sys
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Error_Handling function
+##############################################
+def handle_error(response , comment):
+    code = response.status_code 
+    if code == 401:
+        print( 'Check your Token!')
+        print(comment)
+        sys.exit(-1)
+    if code < 200 or code >= 300:
+        print('Try again later (status code:' , code , ')')
+        print(comment)
+        if 'cli_error' in response:
+            print(response['cli_error'])
+        sys.exit(-1)
+
+
+# Get object_group "Malicious_IPs" from Fortigate firewall and Print it
+# Output: This function return nothing
+def get_malicious_IP_group_From_FG(fw_info):
+    fg_url = "https://%s/api/v2/cmdb/firewall/addrgrp/Malicious_IPs?with_meta=1&datasource=1&skip=1&vdom=%s"  %(fw_info['url'] ,fw_info['vdom'])
+
+    payload={}
+
+    headers = {
+        'Authorization': 'Bearer '+ fw_info["token"] 
+    }
+
+    response = requests.request("GET", fg_url, headers=headers, data=payload, verify=False)
+    handle_error(response, "Getting Malicious_IPs group_object from FG")
+    res = response.json()["results"][0]["member"]
+    address_list = []
+    for r in res:
+        address_list.append(r["name"])   
+    
+    print(address_list)
