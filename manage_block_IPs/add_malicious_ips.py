2f1089f395e5a27c4b19204cf6255e40f068c9e1
ali.nourollahi <alinourollahi777@gmail.com>
2023-07-16 05:56:48 +0000

    Add error_handling and validation functions

---
+++ manage_block_IPs/add_malicious_ips.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3 
+
+from xmlrpc.client import boolean
+import json
+import requests
+import urllib3
+import ipaddress 
+import sys
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+# Validation Functions
+##############################################
+def is_ip_address_valid(address):
+    try:
+        ip = ipaddress.ip_address(address)
+    except ValueError:
+        print("IP address '{}' is not valid!".format(address)) 
+        sys.exit(-1)
+
+# End of validation functions
+##############################################
+
+
+# Error_Handling function
+##############################################
+def handle_error(response , comment):
+    code = response.status_code 
+    if code == 401:
+        print( 'Check your Token!')
+        print(comment)
+        sys.exit(-1)
+    if code < 200 or code >= 300:
+        print('Try again later (status code:' , code , ')')
+        print(comment)
+        if 'cli_error' in response:
+            print(response['cli_error'])
+        sys.exit(-1)
