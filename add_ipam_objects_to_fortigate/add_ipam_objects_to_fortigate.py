0307a85ab16b740203f12b1439bbd05faa3f7a84
ali.nourollahi <alinourollahi777@gmail.com>
2023-07-16 14:05:49 +0330

    Init commit for "add_ipam_objects_to_fortigate.py"
    Add error_handling function

---
+++ add_ipam_objects_to_fortigate/add_ipam_objects_to_fortigate.py
@@ -0,0 +1,24 @@
+from os import confstr
+import requests
+import json
+from ipaddress import IPv4Address
+from ipaddress import IPv4Network
+import urllib3
+import sys
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Error_Handling function
+##############################################
+def handle_error(response , comment):
+    code = response.status_code 
+    if code == 401:
+        print( 'Check your Token!')
+        print(comment)
+        sys.exit(-1)
+    if code < 200 or code >= 300:
+        print('Try again later (status code:' , code , ')')
+        print(comment)
+        if 'cli_error' in response:
+            print(response['cli_error'])
+        sys.exit(-1)
+
