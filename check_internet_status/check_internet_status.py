0c43e7ecd28a354fa62f953dcbbf6b53096065e7
ali.nourollahi <alinourollahi777@gmail.com>
2023-07-30 15:12:43 +0330

    Add get_dst_interface and get_zone function

--- check_internet_status/check_internet_status.py
+++ check_internet_status/check_internet_status.py
@@ -72,3 +72,70 @@ def check_policy(fw_info, wan_interface):
 
     get_address_of_vms(fw_info, VMs)
 
+    
+# Get destination interface based on IP
+# This function searchs for the giving IP in routing monitor to find the outgoing interface
+# Output: Destination interface
+def get_dst_interface(fw_info, ip):
+
+    fg_url = "https://%s/api/v2/monitor/router/lookup?destination=%s&vdom=%s" %(fw_info['url'],ip ,fw_info['vdom'])
+    myToken = fw_info['token']
+    payload={}
+
+    headers = {
+        'Authorization': 'Bearer '+ myToken 
+    }
+    response = requests.request("GET", fg_url, headers=headers, data=payload, verify=False)
+    handle_error(response, "Getting destination interface from FG")
+
+    routes = response.json()['results']
+    
+    return routes["interface"]
+
+
+# Get the zone of an interface
+# This function searchs in zones to find the zone of the giving interface
+# Output: Zone of the giving interface
+def get_zone(fw_info, intf):
+    fg_url = "https://%s/api/v2/cmdb/system/zone?datasource=1&with_meta=1&vdom=%s" %(fw_info['url'], fw_info['vdom'])
+    
+    headers = {
+        'Authorization': 'Bearer '+ fw_info["token"] 
+    }
+
+    payload = {}
+    response = requests.request("GET", fg_url, headers=headers, data=payload, verify=False)
+    handle_error(response, "Getting zone of interface from FG")
+
+    zones = response.json()["results"]
+
+    for z in zones:
+        interfaces = z["interface"]
+        for i in interfaces:
+            if i["interface-name"] == intf:
+                return z["name"]
+
+    return -1
+
+def main():
+    
+    url = input("Enter fortigate url\n")
+    vdom = input("Enter VDOM\n")
+    token = input("Enter token\n")
+
+    fw_info = {
+        "site": url,
+        "vdom": vdom,
+        "token": token
+    }
+
+    wan_interface = get_dst_interface(fw_info, '8.8.8.8')
+    wan_zone = get_zone(fw_info, wan_interface)
+
+    if(wan_zone == -1):
+        wan_zone = wan_interface
+
+    check_policy(fw_info, wan_interface)
+
+if __name__ == "__main__":
+    main()
