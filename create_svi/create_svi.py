db5551ea4b9b04ae0078353e9a28044c4641ffcf
ali.nourollahi <alinourollahi777@gmail.com>
2023-08-14 14:31:59 +0330

    Update main function

--- create_svi/create_svi.py
+++ create_svi/create_svi.py
@@ -22,3 +22,32 @@ def handle_error(response , comment):
         if 'cli_error' in response:
             print(response['cli_error'])
         sys.exit(-1)
+
+def main():
+    site = input()
+    vdom = input()
+    token = input()
+
+    interface = input()
+    alias = input()
+    vid = input()
+    ip = input()
+
+    is_vid_valid(vid)
+    is_ip_address_valid(ip)
+
+    fw_info = {
+        "site": site,
+        "vdom": vdom,
+        "token": token
+    }
+
+    add_interface_to_FG(fw_info, interface, alias, vid, ip)
+
+    interface_name = "VID-" + vid
+    add_zone_to_FG(fw_info, alias, interface_name)
+
+
+if __name__ == "__main__":
+    main()
+
