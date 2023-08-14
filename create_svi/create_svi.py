f8b20e7e59c8ae974273431e188ddd5f4490640f
ali.nourollahi <alinourollahi777@gmail.com>
2023-08-14 14:26:37 +0330

    Init commit for create_svi.py

---
+++ create_svi/create_svi.py
@@ -0,0 +1,24 @@
+from os import confstr
+import requests
+import json
+import ipaddress
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import sys
+
+
+
+# Error_Handling function
+##############################################
+def handle_error(response , comment):
+    code = response.status_code 
+    if code == 401:
+        print( 'Check your Token!')
+        print(comment)
+        sys.exit(-1)
+    if code < 200 or code >= 300:
+        print('Try again later (status code:' , code , ')')
+        print(comment)
+        if 'cli_error' in response:
+            print(response['cli_error'])
+        sys.exit(-1)
