5354d32a1d1e8ea8d16493c8bfdb7b303e63a476
ali.nourollahi <alinourollahi777@gmail.com>
2023-08-14 14:35:55 +0330

    Add validation functions

--- create_svi/create_svi.py
+++ create_svi/create_svi.py
@@ -6,8 +6,6 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import sys
 
-
-
 # Error_Handling function
 ##############################################
 def handle_error(response , comment):
@@ -22,6 +20,39 @@ def handle_error(response , comment):
         if 'cli_error' in response:
             print(response['cli_error'])
         sys.exit(-1)
+
+# Validation functions
+##############################################
+def is_ip_address_valid(address):    
+    try:
+        ip_network = ipaddress.IPv4Network(address, strict=False)
+        ip = address.split('/')[0]
+
+        mask = str(ip_network.netmask)
+        if mask == "255.255.255.255":
+            print("Enter IP with mask. e.g: 192.168.1.254/24")
+            sys.exit(-1)
+
+        if ip == str(ip_network.broadcast_address) or ip == str(ip_network.network_address):
+            print("Invalid IP!")
+            sys.exit(-1)
+
+        
+    except ValueError:
+        print("'{}' is not a valid IP address!".format(address))
+        sys.exit(-1)
+
+def is_vid_valid(vid):
+    try:
+        vid = int(vid)    
+        if(vid < 2 or vid > 4095):
+            print("'{}' is not a valid VID!".format(vid))
+            sys.exit(-1)
+    except ValueError:
+        print("VID '{}' is not a valid VID!".format(vid))
+        sys.exit(-1)
+# End of Validation functions
+##############################################
 
 def main():
     site = input()
